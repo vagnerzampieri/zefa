5c0359cfc26d61029be5552d8a2aef87d4a83a2b
Vagner Zampieri <vgzampieri@gmail.com>
2013-02-05 22:28:22 -0200

    Specs from model balance

--- spec/models/balance_spec.rb
+++ spec/models/balance_spec.rb
@@ -2,4 +2,46 @@ require 'spec_helper'
 
 describe Balance do
   it { expect belong_to(:user)}
+
+  before do
+    @user = User.create(
+      name: "Vagner Zampieri",
+      email: "vgzampieri@gmail.com",
+      password: 'zampieri',
+      password_confirmation: 'zampieri'
+    )
+
+    @balance_negative = Balance.building price: '- 10.00', user_id: @user.id
+    @balance_positive = Balance.building price: '+ 10.00', user_id: @user.id
+    @balance_unsigned = Balance.building price: '10.00', user_id: @user.id
+  end
+
+  it "price only" do
+    expect @balance_negative.price == 10.00
+    expect @balance_positive.price == 10.00
+    expect @balance_unsigned.price == 10.00
+  end
+
+  it "with signal negative" do
+    expect @balance_negative.signal == '-'
+  end
+
+  it "with signal positive" do
+    expect @balance_positive.signal == '+'
+    expect @balance_unsigned.signal == '+'
+  end
+
+  it "with color red" do
+    expect @balance_negative.color == '#FF0000'
+  end
+
+  it "with color green" do
+    expect @balance_positive.color == '#008B00'
+    expect @balance_unsigned.color == '#008B00'
+  end
+
+  it "with other caracter, signal positive" do
+    balance = Balance.building price: '* 10.00', user_id: @user.id
+    expect balance.signal == '+'
+  end
 end
