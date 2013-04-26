2bb8e08b7d8909d8e592116588b6f86036015314
Vagner Zampieri <vgzampieri@gmail.com>
2013-04-26 19:18:37 -0300

    Layout

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module Zefa
     config.encoding = "utf-8"
     config.filter_parameters += [:password]
     config.active_support.escape_html_entities_in_json = true
+    config.time_zone = "Brasilia"
+    config.i18n.default_locale = :"pt-BR"
+    config.active_record.default_timezone = :local
     config.assets.enabled = true
     config.assets.version = '1.0'
   end
